51fa5006772e874ae42362382fc62da87c432847
jhwoodward <jw@vsys.co.uk>
2016-12-17 19:11:36 +0000

    pop tune

--- songs/pop.js
+++ songs/pop.js
@@ -3,24 +3,22 @@ var utils = require('../src/utils');
 var loop = utils.loop;
 
 var melody = ['',
-	`12,1: C/b-bg/-BB `,
-  '24,1: C/b-bg/-BB  C/b-bg/-BB 24,1: C/b-bg/-BB 24,1: C/b-bg/-BB'
+	`24,1: C/b-bg/-BB `
 	];
 
 var harmony = ['',
-	`12,1: E/-edb/D-E `,
-  '24,1: C/b-bg/-BB  C/b-bg/-BB 24,1: C/b-bg/-BB 24,1: C/b-bg/-BB'
+	`24,1: E/-edb/D-E `,
+  `12,1: E//-e//d//b//D/-E/ `,
 	];
 
-var bass = ['',
- 	`24,-1:cGgG cGgG cGgG ccDE`
+var octaves = ['',
+  `12,2: cC^c C^cC ^cC^ cC^c`
+  ];
 
- 	];
+var part1 = loop(melody[1],8);
+var part2 = loop(harmony[1],2) + loop(harmony[2],6);
+var part3 = utils.barRest(4) + loop(octaves[1], 4);
 
-var part1 = loop(melody[1],4);
-var part2 = loop(harmony[1],4);
-var part3 = loop(bass[1], 4);
-
-var song = { name:'pop', parts: [part1,part2] };
+var song = { name:'pop', parts: [part1,part2,part3] };
 //console.log(song);
 play(song, 'IAC Driver Bus 1');

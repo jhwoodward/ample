2115560230a2b8255bee80ed0f57bf7455787c7a
jhwoodward <jw@vsys.co.uk>
2016-12-19 17:34:24 +0000

    removed unused libs

--- songs/pop.js
+++ songs/pop.js
@@ -14,17 +14,19 @@ var harmony = ['',
 	];
 
 var octaves = ['',
-  `12,1:'c'd'e'fga`,//^cC^ cC^c C^cC ^cC^ `
    `12,1:cC^c C^cC ^cC^ cC^c Cc^C `
-   
   ];
 
+var bass = ['',
+  `12,3:'cd'efga`
+]
 
-var part1 = loop(melody[1],8);
-var part2 = loop(harmony[1],2) + loop(harmony[2],6);
-var part3 = barRest(4) + loop(octaves[2], 4);
 
-var song = { name:'pop', parts: [part1,part2,part3] };
+var part1 = loop(melody[1] + barRest(3),8);
+var part2 = loop(harmony[1]  + barRest(2),2) + loop(harmony[2],6);
+var part3 = barRest(4) + loop(octaves[1], 4);
+var part4 = barRest(2) + loop(bass + barRest(2),8);
+var song = { name:'pop', parts: [part1,part2,part3,part4] };
 
 
 

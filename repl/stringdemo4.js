30a3933066861cc12b0952a17ee20a1070f6fdfe
jhwoodward <jw@vsys.co.uk>
2017-02-19 12:47:29 +0000

    piano roll

--- repl/stringdemo4.js
+++ repl/stringdemo4.js
@@ -13,19 +13,19 @@ var rules = {
   'p1a': '1:24, {spiccato}  eGeGeGeG  fGfGfGfG eGeGeGeG {pizzicato} fGfGfGfG eGeGeGeG  fGfGfGfG  ',
   'p2a': '1:24, {spiccato}  cbCbCbCb  DcDcDcDc cbCbCbCb {pizzicato}  DcDcDcDc cbCbCbCb  DcDcDcDc',
   'p3a': '0:36, {spiccato}  gAB gAB gAB gAB  gAB gAB gAB BCa BCa BCa BCa ',
-  'p4a': '-1:48,{spiccato}  C/G/    C/G/    -1:C/G/    C/G/  -1:  C/G/  C/G/   -1: C/G/ C/G/ ',
-  'violin1': 'p1a p1a p1a p1a p1a',
-  'violin2': 'p2a p2a p2a p2a p2a',
-  'viola': 'p3a p3a p3a p3a p3a',
-  'cello': 'p4a p4a p4a p4a p4a',
+  'p4a': '-1:48,{spiccato}  C^G^    C^G^    -1:C^G^    C^G^  -1:  C^G^  C^G^   -1: C^G^ C^G^ ',
+  'violin1': 'p1a p1a p1a p1a p1a ^',
+  'violin2': 'p2a p2a p2a p2a p2a ^',
+  'viola': 'p3a p3a p3a p3a p3a ^',
+  'cello': 'p4a p4a p4a p4a p4a ^',
   //'part5': '48, //// //// //// //// 1:G/// 18, eDFGd//c//g////////6,fe-edca-ad/c/-b/a//24, GC//DE//G-B///12,ag//eD///c//^',
-  'clarinet': '48, 40=C11 //// //// 1:G/// 18, eDFGd//c//g////////6,fe-edca-ad/c/-b/a//24, GC//DE//G-B///12,ag//eD///c//^'
+  'clarinet': '48, 40=C11 //// //// 1:G/// 18, 24,fedcbCDEg///A///B///C///////36,CDEb/CDEc////^' 
 };
 
 var conductor = {
-  0: key.bflat + ' 144=T',
-  16: key.fsharp + ' 113=T',
-  28: key.dmaj + ' 153=T',
+  0: key.cmaj + ' 144=T',
+  16: key.emaj, // + ' 113=T',
+  28: key.dmaj,// + ' 153=T',
   32: key.aflat,
   48: key.dflat,
   64: key.cmaj
--- src/seq.js
+++ src/seq.js
@@ -7,18 +7,44 @@ var pad = require('pad');
 var stopped = false;
 var paused = false;
 var space = '                                                            ';
+var sPaused = '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - P A U S E D - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'.magenta;
+var sStart = '= = = = = = = = = = = = = = = = = = = = = = = = = = = = S T A R T = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ='.yellow;
+var sEnd = '= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = E N D = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ='.red;
+var sStopped = '= = = = = = = = = = = = = = = = = = = = = = = = = = = = = S T O P P E D = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ='.blue;
 function allNotesOff() {
-  for (var i = 0; i < 10; i++) {
-    for (var c = 0; c < 16; c++) {
-      for (var p = 0; p < 128; p++) {
-        output.send('noteoff', {
-          note: p,
-          channel: c
-        });
+
+  setTimeout(offAll, 1);
+  function off() {
+    for (var i = 0; i < 10; i++) {
+      for (var channelKey in noteState) {
+        for (var noteKey in noteState[channelKey]) {
+          if (noteState[channelKey][noteKey]) {
+            output.send('noteoff', {
+              note: noteKey,
+              channel: channelKey
+            });
+            console.log('off: ' + channelKey + ', ' + noteKey)
+          }
+        }
       }
     }
-
   }
+
+  function offAll() {
+    for (var i = 0; i < 3; i++) {
+      for (var c = 0; c < 16; c++) {
+        for (var p = 0; p < 128; p++) {
+          output.send('noteoff', {
+            note: p,
+            channel: c
+          });
+        }
+      }
+
+    }
+  }
+
+
 }
 
 function onKeyPress(ch, key) {
@@ -35,89 +61,41 @@ function onKeyPress(ch, key) {
 }
 
 var indexed = {};
+
+var noteState ;
+function resetNoteState() {
+  noteState = {};
+  for (var i = 0; i < 16; i++) {
+    noteState[i] = {};
+    for (var n = 0; n < 128; n++) {
+      noteState[i][n] = false;
+    }
+  }
+}
+
 var interval = 10;
-var tick = -1, endTick, maxTick, lastBeat;
+var tick = -1, endTick, maxTick, lastBeat, prLastBeat;
+var timeout;
 function onTick() {
   tick++;
 
   if (tick === 0) {
     console.log('\n');
-    console.log(space.bgGreen);
+    console.log(sStart);
   }
 
   indexed[tick].forEach(function (event) {
 
-    var log = '';
-    var data = [];
-    var color;
-
-    var name = event.type;
-
-    switch (event.type) {
-      case 'noteon':
-
-        data.push(event.char);
-        data.push(`p${event.pitch}`);
-
-        if (event.keyswitch) {
-          name = 'keyswitch';
-          color = colors.red;
-        } else {
-          color = colors.yellow;
-          data.push(`v${event.velocity}`);
-        }
-
-
-
-        break;
-      case 'noteoff':
-        if (event.keyswitch) {
-          name = 'ks off';
-        }
-        color = colors.grey;
-        data.push(event.char);
-        data.push(`p${event.pitch}`);
-        data.push(`dur${event.duration}`);
-        break;
-      case 'pitch':
-        color = colors.red;
-        data.push(event.value);
-        break;
-      case 'cc':
-        color = colors.red;
-        data.push(`cc${event.controller}`);
-        data.push(`${event.value}`);
-        break;
-      case 'tempo':
-        color = colors.blue;
-        data.push(event.tempo);
-        break;
+    if (event.type === 'noteon') {
+      noteState[event.channel][event.pitch] = true;
     }
 
-    var beat = Math.round(event.tick / 48);
-    if (lastBeat !== beat) {
-      log += colors.bgBlue(pad(5, beat.toString())) + '  ';
-    } else {
 
-      log += colors.gray(pad(5, beat.toString())) + '  ';
-    }
 
-    if (event.channel) {
-      log += colors.blue(pad((event.channel + 1).toString(), 3));
-    } else {
-      log += '   ';
-    }
-
-    log += color(pad(name, 10)) + '  ';
-    log += color(pad(data.join('  '), 15));
-    if (event.info) log += colors.gray(event.info) + '  ';
-    console.log(log);
-
-    lastBeat = beat;
+    //  logInfo(event);
 
     if (event.type === 'tempo') {
       interval = 1000 / (event.tempo * 0.8);
-
     } else if (!event.sent) {
       output.send(event.type, {
         value: event.value,
@@ -129,19 +107,152 @@ function onTick() {
       event.sent = true;
     }
 
+    if (event.type === 'noteoff') {
+      noteState[event.channel][event.pitch] = false;
+    }
 
   });
 
+  if (tick % 12 === 0) {
+    logPianoRoll();
+  }
+
+
   if (tick < maxTick && (!endTick || tick < endTick) && !stopped && !paused) {
-    setTimeout(onTick, interval);
+    timeout = setTimeout(onTick, interval);
   } else if (!paused) {
     if (endTick < maxTick) {
       api.stop();
     } else if (!stopped) {
-      console.log(space.bgGreen);
-      stopped = true;
+      api.end();
     }
   }
+}
+function logInfo(event) {
+
+
+  var name = event.type;
+  var data = [];
+  var color;
+  var log = '';
+  var beat = Math.round(event.tick / 48);
+
+  switch (event.type) {
+    case 'noteon':
+      data.push(event.char);
+      data.push(`p${event.pitch}`);
+      if (event.keyswitch) {
+        name = 'keyswitch';
+        color = colors.red;
+      } else {
+        color = colors.yellow;
+        data.push(`v${event.velocity}`);
+      }
+      break;
+    case 'noteoff':
+      if (event.keyswitch) {
+        name = 'ks off';
+      }
+      color = colors.grey;
+      data.push(event.char);
+      data.push(`p${event.pitch}`);
+      data.push(`dur${event.duration}`);
+      break;
+    case 'pitch':
+      color = colors.red;
+      data.push(event.value);
+      break;
+    case 'cc':
+      color = colors.red;
+      data.push(`cc${event.controller}`);
+      data.push(`${event.value}`);
+      break;
+    case 'tempo':
+      color = colors.blue;
+      data.push(event.tempo);
+      break;
+  }
+
+  if (lastBeat !== beat) {
+    log += colors.white(pad(5, beat.toString())) + '  ';
+  } else {
+
+    log += colors.gray(pad(5, beat.toString())) + '  ';
+  }
+
+  lastBeat = beat;
+  if (event.channel) {
+    log += colors.blue(pad((event.channel + 1).toString(), 3));
+  } else {
+    log += '   ';
+  }
+
+  log += color(pad(name, 10)) + '  ';
+  log += color(pad(data.join('  '), 15));
+  if (event.info) log += colors.gray(event.info) + '  ';
+  console.log(log);
+
+
+
+}
+
+
+function logPianoRoll() {
+
+  var beat = Math.round(tick / 48);
+
+  var showBeat = !prLastBeat || beat !== prLastBeat;
+  prLastBeat = beat;
+
+
+
+  var pianoRoll = [];
+  for (var channelKey in noteState) {
+    for (var noteKey in noteState[channelKey]) {
+      if (noteState[channelKey][noteKey]) {
+        pianoRoll[noteKey] = channelKey;
+      }
+    }
+  }
+
+  var pianoRollLog = pad(showBeat ? beat.toString() : '', 3).white;
+  for (var i = 0; i < 128; i++) {
+    if (pianoRoll[i]) {
+      var channel = parseInt(pianoRoll[i], 10) + 1;
+      switch (channel) {
+        case 0:
+          pianoRollLog += channel.toString().black.bgWhite;
+          break;
+        case 1:
+          pianoRollLog += channel.toString().black.bgYellow;
+          break;
+        case 2:
+          pianoRollLog += channel.toString().black.bgCyan;
+          break;
+        case 3:
+          pianoRollLog += channel.toString().black.bgGreen;
+          break;
+        case 4:
+          pianoRollLog += channel.toString().black.bgBlue;
+          break;
+        case 5:
+          pianoRollLog += channel.toString().black.bgRed;
+          break;
+        case 5:
+          pianoRollLog += channel.toString().black.bgMagenta;
+          break;
+        default:
+          pianoRollLog += channel.toString().black.bgWhite;
+          break;
+      }
+
+    } else {
+
+      pianoRollLog += '.'.grey;
+    }
+  }
+
+  console.log(pianoRollLog);
 }
 
 var api = {
@@ -149,10 +260,16 @@ var api = {
     var wasPlaying = !stopped;
     stopped = true;
     allNotesOff();
-    console.log(space.bgBlue);
+    console.log(sStopped);
+    console.log('\n');
     process.stdin.removeListener('keypress', onKeyPress);
 
     return wasPlaying;
+  },
+  end: function () {
+    console.log(sEnd);
+    console.log('\n');
+    stopped = true;
   },
   togglePause: function () {
     paused = !paused;
@@ -160,16 +277,16 @@ var api = {
       setTimeout(onTick, interval);
     } else {
       allNotesOff();
-      console.log(space.bgMagenta);
+      console.log(sPaused);
     }
 
   },
   start: function (events, startBeat, endBeat) {
-    //keypress(process.stdin);
+    resetNoteState();
+    clearTimeout(timeout);
+    process.stdin.removeListener('keypress', onKeyPress);
     process.stdin.on('keypress', onKeyPress);
-
     var startTick = -1;
-
     if (startBeat) {
       startTick = (parseInt(startBeat, 10) * 48) - 1;
     }
@@ -196,7 +313,7 @@ var api = {
       });
     }
 
-    setTimeout(onTick, interval);
+    timeout = setTimeout(onTick, interval);
 
   }
 };
